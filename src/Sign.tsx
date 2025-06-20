8260699a031a000ed9151abb10cb071478157ee8
Wonhee Lee <2wheeh@gmail.com>
2025-06-20 11:27:37 +0900

    feat: basic cw20 signer

---
+++ src/Sign.tsx
@@ -0,0 +1,44 @@
+import { useConnectedWallet, type SignResult } from '@xpla/wallet-provider';
+import { MsgExecuteContract, SignMode, type CreateTxOptions } from '@xpla/xpla.js';
+import { useState } from 'react';
+
+const cw20Contract = 'xpla1xljvdrtyn86kv7hrdhae4qxdy8krajah3w7xhtyrt0n69und9xdqdhrasc'; // Replace it with the address of the CW20 token created in example-4.js.
+
+export function Sign() {
+  const wallet = useConnectedWallet();
+  const [signed, setSigned] = useState<SignResult | null>(null);
+  const sign = async () => {
+    if (!wallet) {
+      return;
+    }
+
+    const transferMsg = new MsgExecuteContract(wallet.walletAddress, cw20Contract, {
+      transfer: {
+        recipient: 'xpla1ek9fpjx5qrga6ajp0lk5akwm4s24twmzhc9725',
+        amount: '0',
+      },
+    });
+
+    const createTxOptions = {
+      memo: '',
+      msgs: [transferMsg],
+    } satisfies CreateTxOptions;
+
+    const res = await wallet.sign({ ...createTxOptions, signMode: SignMode.SIGN_MODE_LEGACY_AMINO_JSON });
+    setSigned(res);
+  };
+
+  return (
+    <>
+      <button onClick={sign} disabled={!wallet}>
+        {wallet ? 'Sign' : 'Connect Wallet First'}
+      </button>
+      {signed && (
+        <div>
+          <h2>Signed Result</h2>
+          <pre>{JSON.stringify(signed, null, 2)}</pre>
+        </div>
+      )}
+    </>
+  );
+}
--- src/Wallet.tsx
+++ src/Wallet.tsx
@@ -1,11 +1,13 @@
 import { Connect } from './Connect';
 import { Send } from './Send';
+import { Sign } from './Sign';
 
 export function Wallet() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
       <Connect />
       <Send />
+      <Sign />
     </div>
   );
 }
